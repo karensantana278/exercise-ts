8b82efa2a7e8f5201f4a3751e7cb4035a1512458
karen.santana <karen.santana@alpes.one>
2024-01-08 14:55:15 -0300

    add typescript exercise

---
+++ src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(number1: number, number2: number): number{
+    return number1 * number2;
+}
+
+function saudar(nome: string): string{
+    return "Olá " + nome
+}
+
+console.log(multiplicar(5, 6))
+console.log(saudar('Myllena'))
